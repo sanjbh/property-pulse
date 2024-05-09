6290e6017d1dd04e7b09c60169749dbb92920665
Sanjay Bhattacharya <sanjbh@gmail.com>
2024-05-09 17:35:57 +0530

    Added navbar

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useParams } from "next/navigation";
+
+const PropertyPage = () => {
+  return <div>PropertyPage</div>;
+};
+
+export default PropertyPage;
